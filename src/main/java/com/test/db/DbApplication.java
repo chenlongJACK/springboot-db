126322848bd299493b0c06f6cfe022a7a9072aed
chenlong <951709483@qq.com>
2020-08-17 17:29:35 +0800

    数据库

---
+++ src/main/java/com/test/db/DbApplication.java
@@ -0,0 +1,17 @@
+package com.test.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenlong
+ * @version 1.0
+ * @description 启动类
+ * @date
+ */
+@SpringBootApplication
+public class DbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbApplication.class,args);
+    }
+}
